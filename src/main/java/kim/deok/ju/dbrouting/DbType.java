766c4701cfe18b6279fe5fa2430c84d47e09341d
kimdeokju <darkduk@naver.com>
2017-06-02 14:09:34 +0900

    DbContextHolder 하드코딩 부분을 제거

--- src/main/java/kim/deok/ju/dbrouting/DbType.java
+++ src/main/java/kim/deok/ju/dbrouting/DbType.java
@@ -1,7 +1,22 @@
 package kim.deok.ju.dbrouting;
 
 public enum DbType {
-   MASTER,
-   SLAVE,
-   USERDB,
+   MASTER(1),
+   SLAVE(2),
+   USERDB(3);
+
+   final private int dbNum;
+
+   DbType(int dbNum) {
+      this.dbNum = dbNum;
+   }
+
+   public static DbType getDbType(int dbNum){
+      for(DbType dbType : DbType.values()) {
+         if(dbType.dbNum == dbNum) {
+            return dbType;
+         }
+      }
+      return null;
+   }
 }
